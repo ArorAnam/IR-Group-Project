f3e98e86b10eef9f8ccea8ab0b0499a95d10ff37
Naman Arora <59131920+ArorAnam@users.noreply.github.com>
2023-11-25 13:42:46 +0000

    start fbi modelling

---
+++ src/main/java/org/example/model/FbisModel.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+/*
+We are looking for these fields:
+Most Important:
+<TEXT> <DCONO>
+
+Others taken into consideration:
+<DATE1>
+<TXT5>
+<HEADER>
+<F ...>
+ */
+
+public class FbisModel {
+
+}
