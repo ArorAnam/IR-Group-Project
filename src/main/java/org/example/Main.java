795a071fa3e16086eeaa3b7bdd820ec81a864748
Naman Arora <59131920+ArorAnam@users.noreply.github.com>
2023-11-25 17:23:24 +0000

    update indexer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
 import org.apache.lucene.document.Document;
+import org.example.indexer.FbisIndexer;
 import org.example.indexer.Fr94Indexer;
+import org.example.model.FbisModel;
 import org.example.model.TopicModel;
+import org.example.parser.FbisParser;
 import org.example.parser.Fr94Parser;
 import org.example.parser.TopicParser;
 
@@ -16,12 +19,21 @@ public class Main {
         //System.out.println(temp1.loadTopics());
 
         // Get Fr94 Models
-        //Fr94Parser temp = new Fr94Parser();
-        //System.out.println(temp.getData());
+        //Fr94Parser frParser = new Fr94Parser();
+        //System.out.println(frParser.getData());
 
         // Get Fr94 documents
-        //Fr94Indexer temp = new Fr94Indexer();
-        //ArrayList<Document> docs = temp.getDocuments();
+        //Fr94Indexer fr9rdocs = new Fr94Indexer();
+        //ArrayList<Document> docs = fr9rdocs.getDocuments();
         //System.out.println(docs.get(1));
+
+        // Get fbis models
+        //FbisParser temp2 = new FbisParser();
+        //System.out.println(temp2.getData());
+
+        //get fbis documents
+        //FbisIndexer temp3 = new FbisIndexer();
+        //ArrayList<Document> fbisdocs = temp3.getDocuments();
+        //System.out.println(fbisdocs.get(1));
     }
 }
